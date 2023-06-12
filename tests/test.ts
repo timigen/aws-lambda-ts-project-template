5a0264de1f4295901934b2bc873b33d39b389a2c
Timigen Khan <timigen@gmail.com>
2023-06-12 04:06:58 -0400

    run formatter

--- tests/test.ts
+++ tests/test.ts
@@ -1,7 +1,7 @@
-import { handler } from "../src/handler"
+import { handler } from "../src/handler";
 
 test("handler response is not null", () => {
-  handler(null).then((resp)=>{
+  handler(null).then((resp) => {
     expect(resp !== null).toBe(true);
   });
 });
